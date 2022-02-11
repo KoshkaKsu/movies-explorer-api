dd39831ba37eb445f76cfec4494a7214883460bb
KoshkaKsu <ksenia.kuzn@mail.ru>
2022-02-11 19:52:53 +0300

    fix:исправление ошибок после ревью

--- backend/utils/rateLimiter.js
+++
@@ -1,9 +0,0 @@
-const rateLimit = require('express-rate-limit');
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 1000,
-  message: 'Превышен лимит запросов на сервер!',
-});
-
-module.exports = limiter;
