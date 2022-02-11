3dcf6e08309afc7f89fac0ff998967b3f2223ff3
KoshkaKsu <ksenia.kuzn@mail.ru>
2022-02-11 20:25:42 +0300

    fix:исправление ошибок после ревью

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/unauthorized-err');
+const messages = require('../errors/errors-mesage');
 
 const { NODE_ENV, JWT_SECRET_KEY } = process.env;
 
@@ -7,7 +8,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnauthorizedError('Необходимо авторизоваться');
+    throw new UnauthorizedError(messages.unauthorizedError);
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -16,7 +17,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET_KEY : 'dev-secret');
   } catch (err) {
-    throw new UnauthorizedError('Необходимо авторизоваться!');
+    throw new UnauthorizedError(messages.unauthorizedError);
   }
 
   req.user = payload;
